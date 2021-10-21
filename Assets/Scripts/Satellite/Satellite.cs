f80b6b259c21bfdb256eea125b3363bf2a2f3783
Kim Hyeonwoo <1reds@naver.com>
2021-10-22 03:04:37 +0900

    Add Satellite Game
    1. 인공위성 게임 추가
    2. 각 Game별 폴더 생성 및 파일 위치 이동

---
+++ Assets/Scripts/Satellite/Satellite.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Satillite
+{
+    public class Satellite : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject mainPlanet;
+
+        [SerializeField]
+        private GameObject sateliteScore;
+
+        private Quaternion sateliteScoreOriginRotation;
+
+        private void Start()
+        {
+            sateliteScoreOriginRotation = sateliteScore.transform.rotation;
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            GetComponent<RectTransform>().RotateAround(mainPlanet.GetComponent<RectTransform>().position, -transform.forward, 20 * Time.deltaTime);
+            sateliteScore.transform.rotation = sateliteScoreOriginRotation;
+        }
+    }
+}

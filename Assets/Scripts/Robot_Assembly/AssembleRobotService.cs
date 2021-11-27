d312111cd1423e6fb5d5a126fdc1ee5ddb2297e9
Kim Hyeonwoo <1reds@naver.com>
2021-11-27 23:02:29 +0900

    Mod Assemt_Robot Scene
    1. Canvas를 Expand로 설정
    2. AssembleRobotService 추가

---
+++ Assets/Scripts/Robot_Assembly/AssembleRobotService.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+using UniRx;
+
+public class AssembleRobotService : MonoBehaviour
+{
+    [SerializeField]
+    private Button homeButton;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        BindView();
+    }
+
+    private void BindView()
+    {
+        homeButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                SceneService.Instance.LoadScene("Logo");
+            })
+            .AddTo(gameObject);
+    }
+}

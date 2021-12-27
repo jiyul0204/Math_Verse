58c6c4bb2907bc4efec1ff8f572618c5290b55d0
jiyul0204 <87352553+jiyul0204@users.noreply.github.com>
2021-12-27 22:28:51 +0900

    .

--- Assets/Scripts/Robot_Assembly/APIManager.cs
+++ Assets/Scripts/Robot_Assembly/APIManager.cs
@@ -1,54 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using System.IO;
 
-[System.Serializable]
-public class Data
+public class EducationInform
 {
-    public int m_nLevel;
-    public Vector3 m_vecPositon;
-
-    public void printData()
-    {
-        Debug.Log("Level : " + m_nLevel);
-        Debug.Log("Position : " + m_vecPositon);
-    }
+    public string ID;
+    public string Date;
+    public bool Correct;
+    public int timeSec;
 }
+
 public class APIManager : MonoBehaviour
 {
-    void Start()
+    public static string GetCurrentDate()
     {
-        Data data = new Data();
-        data.m_nLevel = 12;
-        data.m_vecPositon = new Vector3(3.4f, 5.6f, 7.8f);
-
-        string str = JsonUtility.ToJson(data);
-
-        Debug.Log("ToJson : " + str);
-
-        Data data2 = JsonUtility.FromJson<Data>(str);
-        data2.printData();
-
-        // file save 
-
-        Data data3 = new Data();
-        data3.m_nLevel = 99;
-        data3.m_vecPositon = new Vector3(8.1f, 9.2f, 7.2f);
-
-        File.WriteAllText(Application.dataPath + "/Robot_Assemble.json", JsonUtility.ToJson(data3));
-
-        // file load 
-
-        string str2 = File.ReadAllText(Application.dataPath + "/Robot_Assemble.json");
-
-        Data data4 = JsonUtility.FromJson<Data>(str2);
-        data4.printData();
+        return DateTime.Now.ToString(("yyyy-MM-dd HH:mm:ss tt"));
     }
 
-    // Update is called once per frame 
-    void Update()
-    {
+    EducationInform EduInform = new EducationInform();
+    string strFilename = "Robot_GameData";
 
+    string strDate;
+    bool IsCorrect;
+    int nTimeSec;
+
+    void SaveData()
+    {
+        string json = JsonUtility.ToJson(EduInform);
+        string strPath = Application.dataPath + "/" + strFilename + ".Json";
+        File.WriteAllText(strPath, json);
+    }
+
+    public void GetData( bool IsCorrect, int nSec)
+    {
+        EduInform.ID = "ID";
+        EduInform.Date = GetCurrentDate();
+        EduInform.Correct = IsCorrect;
+        EduInform.timeSec = nSec;
+    }
+
+    public void LoadData()
+    {
+        string strPath = Application.dataPath + "/" + strFilename + ".Json";
+        string json = File.ReadAllText(strPath);
+        EducationInform EduInform = JsonUtility.FromJson<EducationInform>(json);
+
+        strDate = EduInform.Date;
+        IsCorrect = EduInform.Correct;
+        nTimeSec = EduInform.timeSec;
+    }
+
+    public string Date()
+    {
+        return strDate;
+    }
+    public bool Correct()
+    {
+        return IsCorrect;
+    }
+
+    public int TimeSec()
+    {
+        return nTimeSec;
     }
 }

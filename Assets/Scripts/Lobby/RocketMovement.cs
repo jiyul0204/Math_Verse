1072d38872c1214b4e672e8b844078a36e6ddb53
Kim Hyeonwoo <1reds@naver.com>
2022-01-19 17:59:26 +0900

    Add DOTween and RocketMovement Script
    1. DOTween Free Version 추가
    2. Rocket 이동 Script 추가

---
+++ Assets/Scripts/Lobby/RocketMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+using DG.Tweening;
+
+public class RocketMovement : MonoBehaviour
+{
+	private Transform target;
+	public PathType pathType = PathType.CatmullRom;
+	public Vector3[] waypoints;
+
+	private void Awake()
+    {
+		target = GetComponent<Transform>();
+    }
+
+    private void Start()
+	{
+		// Create a path tween using the given pathType, Linear or CatmullRom (curved).
+		// Use SetOptions to close the path
+		// and SetLookAt to make the target orient to the path itself
+		Tween t = target.DOPath(waypoints, 4, pathType, PathMode.Sidescroller2D)
+			.SetOptions(false, lockRotation: AxisConstraint.Z)
+			.SetLookAt(0.001f);
+
+		// Then set the ease to Linear and use infinite loops
+		t.SetEase(Ease.Linear).SetLoops(-1);
+	}
+}

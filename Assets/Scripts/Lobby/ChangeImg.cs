2096c48a915dd19caab00b309c7ddccc8185514c
jiyul0204 <87352553+jiyul0204@users.noreply.github.com>
2022-02-02 15:17:28 +0900

    Update ChangeImg.cs

--- Assets/Scripts/Lobby/ChangeImg.cs
+++ Assets/Scripts/Lobby/ChangeImg.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ChangeImg : MonoBehaviour
 {
-    public Image SprOriginImage; //기존에 존제하는 이미지
-    public Sprite SprChangeImage; //바뀌어질 이미지
+    public Image SprOriginImage; //기존에 존재하는 이미지
+    public Sprite SprOriginSprite; //기존에 존재하는 이미지
+    public Sprite SprChangeSprite; //바뀌어질 이미지
 
-    public void ChangeImage()
+    private void Start()
     {
-        SprOriginImage.sprite = SprChangeImage;
+        if(GetHour()>=23 || GetHour()<=6)
+        {
+            ChangeImage(SprChangeSprite);
+        }
+        else
+        {
+            ChangeImage(SprOriginSprite);
+        }
+    }
+    public void ChangeImage(Sprite img)
+    {
+        SprOriginImage.sprite = img;
     }
  
+    int GetHour()
+    {
+        return Int32.Parse(DateTime.Now.ToString("HH"));
+    }
 }
